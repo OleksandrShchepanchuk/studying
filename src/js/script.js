d8a5fc941e4903bfef21373aa869c708b2fa36bc
Oleksandr <education.oleksandr@gmail.com>
2022-02-12 16:51:39 +0200

    second commit

---
+++ src/js/script.js
@@ -0,0 +1,131 @@
+const slider = tns({
+    container: '.carousel__inner',
+    edgePadding: 20,
+    gutter: 20,
+    nav: true,
+    controls: false,
+    responsive: {
+        768: {
+            items: 1,
+            slideBy: 'page',
+            autoplay: false,
+            controls: false,
+            nav: false,
+            mouseDrag: true
+        }
+    }
+  });
+
+
+
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
+
+(function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+        function toggleSlide(item) {
+            $(item).each(function(i) {
+                $(this).on('click', function(e) {
+                    e.preventDefault();
+                    $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                }) 
+                }) ;
+        };
+        toggleSlide('.catalog-item__link')
+        toggleSlide('.catalog-item__back')
+
+
+        //Modal
+
+        $('[data-modal=consultation]').on('click', function(){
+            $('.overlay, #consultation').fadeIn('slow')
+        });
+        $('.modal__close').on('click', function(){
+            $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+        });
+
+        $('.button_mini').each(function(i) {
+            $(this).on('click', function() {
+                $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+                $('#order, .overlay').fadeIn('slow');
+            })
+        });
+
+
+        function validateForms(form) {
+            $(form).validate({
+                rules: {
+                    name: "required",
+                    phone: "required",
+                    email: {
+                        required: true,
+                        email: true
+                    }
+                },
+                messages: {
+                    name: "Будь ласка, введіть своє ім'я",
+                    phone: "Будь ласка, введіть свій номер телефону",
+                    email: {
+                      required: "Будь ласка, введіть свою пошту",
+                      email: "Неправильно введена адреса пошти"
+                    }
+                }
+            });
+        };
+        validateForms('#order form')
+        validateForms('#consultation-form')
+        validateForms('#consultation form')
+
+
+        $('input[name=phone]').mask("+38 (999) 999-99-99");
+
+        $('form').submit(function(e){
+            e.preventDefault();
+
+            if (!$(this).valid()) {
+                return;
+            }
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function(){
+                $(this).find("input").val("");
+                $('#consultation, #order').fadeOut();
+                $('.overlay, #thanks').fadeIn('slow');
+                $('form').trigger('reset');
+
+            });
+            return false;
+        })
+       
+       
+        $('.pageup').hide()
+        $(window).scroll(function(){
+            if ($(this).scrollTop() < 1600) {
+                $('.pageup').fadeOut();
+
+            } else {                
+                $('.pageup').fadeIn();
+        }  
+            
+        })
+    });
+
+})(jQuery);
+
+
